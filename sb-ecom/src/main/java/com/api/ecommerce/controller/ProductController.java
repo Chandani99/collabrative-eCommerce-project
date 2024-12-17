66414229ce24aef40d449cb52d5ea2f51c52c830
Rohit Kumar <103635045+Rohit9252@users.noreply.github.com>
2024-12-17 11:25:14 +0530

    Revert "Chandani"

--- sb-ecom/src/main/java/com/api/ecommerce/controller/ProductController.java
+++
@@ -1,7 +0,0 @@
-package com.api.ecommerce.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class ProductController {
-}
--- sb-ecom/src/main/java/com/api/ecommerce/model/AppRole.java
+++
@@ -1,8 +0,0 @@
-package com.api.ecommerce.model;
-
-public enum AppRole {
-    ROLE_USER,
-    ROLE_SELLER,
-    ROLE_ADMIN
-}
-
